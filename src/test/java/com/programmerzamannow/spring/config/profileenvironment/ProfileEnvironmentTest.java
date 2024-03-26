f20dc0779add6e435a91890ea109717aeee64edf
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-26 20:13:00 +0700

    profile

---
+++ src/test/java/com/programmerzamannow/spring/config/profileenvironment/ProfileEnvironmentTest.java
@@ -0,0 +1,42 @@
+package com.programmerzamannow.spring.config.profileenvironment;
+
+import com.programmerzamannow.spring.config.profile.ProfileTest;
+import lombok.Setter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = ProfileEnvironmentTest.TestApplication.class)
+@ActiveProfiles({"production","local"})
+public class ProfileEnvironmentTest {
+
+    @Autowired
+    private TestApplication.SampleProfile sampleProfile;
+
+
+    @Test
+    void testActiveProfile() {
+        Assertions.assertArrayEquals(new String[]{"production","local"}, sampleProfile.getProfile());
+    }
+
+    @SpringBootApplication
+    public static class TestApplication{
+
+        @Component
+        public static class SampleProfile implements EnvironmentAware {
+
+            @Setter
+            private Environment environment;
+
+            public String[] getProfile(){
+                return environment.getActiveProfiles();
+            }
+        }
+    }
+}
